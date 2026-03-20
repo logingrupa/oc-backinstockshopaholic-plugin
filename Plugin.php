8fa6800f84aa81834bd902a8a2b11667b01258ba
roulendz <roulendz@users.noreply.github.com>
2026-03-20 19:05:13 +0200

    Add dependency requirement for Lovata.Shopaholic

--- Plugin.php
+++ Plugin.php
@@ -6,6 +6,9 @@ use System\Classes\PluginBase;
  * Class Plugin
  * @package Logingrupa\BackInStockShopaholic
  */
-class Plugin extends PluginBase
+class Plugin extends \System\Classes\PluginBase
 {
+    public $require = [
+        'Lovata.Shopaholic',
+    ];
 }
